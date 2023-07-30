fd0a19967b62e65db3f6ca4478346ad38c1b4391
samarjit-singh <samarjitbravo@gmail.com>
2023-07-30 13:12:00 +0530

    created check-user api, login and onboarding page, able to login through googleauthprovider

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import AuthRoutes from "./routes/AuthRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -8,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", AuthRoutes);
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
 });
-
