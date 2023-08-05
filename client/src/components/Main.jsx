89cf41ffda1d68352b4e70e60835ae9a1c13060f
samarjit-singh <samarjitbravo@gmail.com>
2023-08-06 02:13:26 +0530

    started working on chatlist and empty

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ChatList from "./Chatlist/ChatList";
+import Empty from "./Empty";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "@/utils/FirebaseConfig";
+import axios from "axios";
+import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
+import { useRouter } from "next/router";
+import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import { data } from "autoprefixer";
 
 function Main() {
-  return <div>Main</div>;
+  const router = useRouter();
+  const [{ userInfo }, dispatch] = useStateProvider();
+  const [redirectLogin, setRedirectLogin] = useState(false);
+
+  useEffect(() => {
+    if (redirectLogin) router.push("/login");
+  }, [redirectLogin]);
+
+  // run when page is loaded
+  onAuthStateChanged(firebaseAuth, async (currentUser) => {
+    //similar to useEffect provided firebase
+    if (!currentUser) setRedirectLogin(true);
+    if (!userInfo && currentUser?.email) {
+      const { data } = await axios.post(CHECK_USER_ROUTE, {
+        email: currentUser.email,
+      });
+      if (!data.status) {
+        router.push("/login");
+      }
+      const { id, name, email, profilePicture: profileImage, status } = data;
+
+      dispatch({
+        type: reducerCases.SET_USER_INFO,
+        userInfo: {
+          id,
+          name,
+          email,
+          profileImage,
+          status,
+        },
+      });
+    }
+  });
+
+  return (
+    <>
+      <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
+        <ChatList />
+        <Empty />
+      </div>
+    </>
+  );
 }
 
 export default Main;
