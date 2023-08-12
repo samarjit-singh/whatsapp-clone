508ac44248c128b5b8bc47fe47eb5f9c78f96485
samarjit-singh <samarjitbravo@gmail.com>
2023-08-12 23:54:42 +0530

    created get all user API

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -41,3 +41,36 @@ export const onBoardUser = async (request, response, next) => {
     next(error);
   }
 };
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const prisma = getPrismaInstance();
+    const users = await prisma.user.findMany({
+      orderBy: { name: "asc" },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        profilePicture: true,
+        about: true,
+      },
+    });
+
+    // group all the users with their inital letters
+
+    const usersGroupedByInitialLetter = {};
+
+    users.forEach((user) => {
+      const initialLetter = user.name.charAt(0).toUpperCase();
+      if (!usersGroupedByInitialLetter[initialLetter]) {
+        usersGroupedByInitialLetter[initialLetter] = [];
+      }
+
+      usersGroupedByInitialLetter[initialLetter].push(user);
+    });
+
+    return res.status(200).send({ users: usersGroupedByInitialLetter });
+  } catch (error) {
+    next(error);
+  }
+};
