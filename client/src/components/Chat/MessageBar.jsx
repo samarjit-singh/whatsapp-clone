79c8dd8806c5423bd97cb3a6702828b3b5afcbd9
samarjit-singh <samarjitbravo@gmail.com>
2023-08-13 13:49:27 +0530

    created prisma schema for messages

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useStateProvider } from "@/context/StateContext";
 import { BsEmojiSmile } from "react-icons/bs";
 import { FaMicrophone } from "react-icons/fa";
 import { ImAttachment } from "react-icons/im";
 import { MdSend } from "react-icons/md";
 
 function MessageBar() {
+  const [{ userInfo, currentChatUser }, dispatch] = useStateProvider();
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async () => {
+    alert(message);
+  };
+
   return (
     <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
       <>
@@ -23,6 +31,8 @@ function MessageBar() {
             type="text"
             placeholder="Type a message"
             className="bg-input-background text-sm focus:outline-none text-white h-10 rounded-lg px-5 py-4 w-full"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
           />
         </div>
         <div className="flex w-10 items-center justify-center">
@@ -30,6 +40,7 @@ function MessageBar() {
             <MdSend
               className="text-panel-header-icon cursor-pointer text-xl"
               title="Send Message"
+              onClick={sendMessage}
             />
             {/* <FaMicrophone
               className="text-panel-header-icon cursor-pointer text-xl"
