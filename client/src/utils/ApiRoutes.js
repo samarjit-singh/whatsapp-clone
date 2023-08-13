cfb6d5ba393b05b5b8cc80f90d0fddd729a71f3b
samarjit-singh <samarjitbravo@gmail.com>
2023-08-13 14:07:13 +0530

    created add message route

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,7 +1,11 @@
 export const HOST = "http://localhost:3005";
 
 const AUTH_ROUTE = `${HOST}/api/auth`;
+const MESSAGES_ROUTE = `${HOST}/api/messages`;
 
 export const onBoardUserRoute = `${AUTH_ROUTE}/onboarduser`;
 export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
 export const GET_ALL_CONTACTS = `${AUTH_ROUTE}/get-contacts`;
+
+
+export const ADD_MESSAGE_ROUTE = `${MESSAGES_ROUTE}/add-message`;
