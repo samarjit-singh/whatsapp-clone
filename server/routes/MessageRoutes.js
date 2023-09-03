9f32b8ef1c65fd5b7fd10a2eb176a4367f61426d
samarjit-singh <samarjitbravo@gmail.com>
2023-09-03 15:27:58 +0530

    displyaing chat list based on latest message

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -4,6 +4,7 @@ import {
   getMessages,
   addImageMessage,
   addAudioMessage,
+  getInitialContactswithMessages,
 } from "../controllers/MessageController.js";
 import multer from "multer";
 
@@ -16,5 +17,6 @@ router.post("/add-message", addMessage);
 router.get("/get-messages/:from/:to", getMessages);
 router.post("/add-image-message", uploadImage.single("image"), addImageMessage);
 router.post("/add-audio-message", upload.single("audio"), addAudioMessage);
+router.get("/get-initial-contacts/:from", getInitialContactswithMessages);
 
 export default router;
