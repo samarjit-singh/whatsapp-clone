ff38c317cfb6a5cfcab70efcaaed2d0b542c911f
samarjit-singh <samarjitbravo@gmail.com>
2023-09-08 01:16:17 +0530

    added searching contact feature in chat list item and contact list

--- client/src/components/Chatlist/List.jsx
+++ client/src/components/Chatlist/List.jsx
@@ -6,7 +6,7 @@ import React, { useEffect } from "react";
 import ChatLIstItem from "./ChatLIstItem";
 
 function List() {
-  const [{ userInfo, userContacts, onlineUsers }, dispatch] =
+  const [{ userInfo, userContacts, filteredContacts }, dispatch] =
     useStateProvider();
 
   useEffect(() => {
@@ -28,9 +28,13 @@ function List() {
 
   return (
     <div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar">
-      {userContacts.map((contact) => (
-        <ChatLIstItem data={contact} key={contact.id} />
-      ))}
+      {filteredContacts && filteredContacts.length > 0
+        ? filteredContacts.map((contact) => (
+            <ChatLIstItem data={contact} key={contact.id} />
+          ))
+        : userContacts.map((contact) => (
+            <ChatLIstItem data={contact} key={contact.id} />
+          ))}
     </div>
   );
 }
--- client/src/context/StateReducers.js
+++ client/src/context/StateReducers.js
@@ -10,6 +10,7 @@ export const initialState = {
   messagesSearch: false,
   userContacts: [],
   onlineUsers: [],
+  filteredContacts: [],
 };
 
 const reducer = (state, action) => {
@@ -63,6 +64,15 @@ const reducer = (state, action) => {
       return {
         ...state,
         onlineUsers: action.onlineUsers,
+      };
+    case reducerCases.SET_CONTACT_SEARCH:
+      const filteredContacts = state.userContacts.filter((contact) =>
+        contact.name.toLowerCase().includes(action.contactSearch.toLowerCase())
+      );
+      return {
+        ...state,
+        contactSearch: action.contactSearch,
+        filteredContacts,
       };
     default:
       return state;
