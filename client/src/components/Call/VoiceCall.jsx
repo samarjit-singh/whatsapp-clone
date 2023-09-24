a0e578cee0cb3154bc97c5c8b16d8d412b2e8d73
samarjit-singh <samarjitbravo@gmail.com>
2023-09-24 14:18:23 +0530

    set up sockets for voice and video call

--- client/src/components/Call/VoiceCall.jsx
+++ client/src/components/Call/VoiceCall.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useStateProvider } from "@/context/StateContext";
+import dynamic from "next/dynamic";
+import React, { useEffect } from "react";
+const Container = dynamic(() => import("./Container"), { ssr: false });
 
 function VoiceCall() {
-  return <div>VoiceCall</div>;
+  const [{ voiceCall, socket, userInfo }] = useStateProvider();
+
+  useEffect(() => {
+    if (voiceCall.type === "out-going") {
+      alert("sending event");
+      socket.current.emit("outgoing-voice-call", {
+        to: voiceCall.id,
+        from: {
+          id: userInfo.id,
+          profilePicture: userInfo.profileImage,
+          name: userInfo.name,
+        },
+        callType: voiceCall.callType,
+        roomId: voiceCall.roomId,
+      });
+    }
+  }, [voiceCall]);
+
+  return <Container data={voiceCall} />;
 }
 
 export default VoiceCall;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -12,11 +12,25 @@ import { data } from "autoprefixer";
 import Chat from "./Chat/Chat";
 import { io } from "socket.io-client";
 import SearchMessages from "./Chat/SearchMessages";
+import VideoCall from "./Call/VideoCall";
+import VoiceCall from "./Call/VoiceCall";
+import IncomingVideoCall from "./common/IncomingVideoCall";
+import IncomingCall from "./common/IncomingCall";
 
 function Main() {
   const router = useRouter();
-  const [{ userInfo, currentChatUser, messagesSearch }, dispatch] =
-    useStateProvider();
+  const [
+    {
+      userInfo,
+      currentChatUser,
+      messagesSearch,
+      videoCall,
+      voiceCall,
+      incomingVoiceCall,
+      incomingVideoCall,
+    },
+    dispatch,
+  ] = useStateProvider();
   const [redirectLogin, setRedirectLogin] = useState(false);
   const [socketEvent, setSocketEvent] = useState(false);
   const socket = useRef();
@@ -78,6 +92,34 @@ function Main() {
           },
         });
       });
+
+      socket.current.on("incoming-voice-call", ({ from, roomId, callType }) => {
+        dispatch({
+          type: reducerCases.SET_INCOMING_VOICE_CALL,
+          incomingVoiceCall: { ...from, roomId, callType },
+        });
+      });
+
+      socket.current.on("incoming-video-call", ({ from, roomId, callType }) => {
+        alert("incoming video call");
+        dispatch({
+          type: reducerCases.SET_INCOMING_VIDEO_CALL,
+          incomingVideoCall: { ...from, roomId, callType },
+        });
+      });
+
+      socket.current.on("voice-call-rejected", () => {
+        dispatch({
+          type: reducerCases.END_CALL,
+        });
+      });
+
+      socket.current.on("video-call-rejected", () => {
+        dispatch({
+          type: reducerCases.END_CALL,
+        });
+      });
+
       setSocketEvent(true);
     }
   }, [socket.current]);
@@ -98,18 +140,37 @@ function Main() {
 
   return (
     <>
-      <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
-        <ChatList />
-        {currentChatUser ? (
-          <div className={messagesSearch ? "grid grid-cols-2" : "grid-cols-2"}>
-            <Chat />
+      {incomingVideoCall && <IncomingVideoCall />}
 
-            {messagesSearch && <SearchMessages />}
-          </div>
-        ) : (
-          <Empty />
-        )}
-      </div>
+      {incomingVoiceCall && <IncomingCall />}
+
+      {videoCall && (
+        <div className="h-screen w-screen max-h-full overflow-hidden">
+          <VideoCall />
+        </div>
+      )}
+      {voiceCall && (
+        <div className="h-screen w-screen max-h-full overflow-hidden">
+          <VoiceCall />
+        </div>
+      )}
+
+      {!videoCall && !voiceCall && (
+        <div className="grid grid-cols-main h-screen w-screen max-h-screen max-w-full overflow-hidden">
+          <ChatList />
+          {currentChatUser ? (
+            <div
+              className={messagesSearch ? "grid grid-cols-2" : "grid-cols-2"}
+            >
+              <Chat />
+
+              {messagesSearch && <SearchMessages />}
+            </div>
+          ) : (
+            <Empty />
+          )}
+        </div>
+      )}
     </>
   );
 }
