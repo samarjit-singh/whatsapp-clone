686be10cf70e69dfa8049e07d8ee2a5ccb89ca13
samarjit-singh <samarjitbravo@gmail.com>
2023-10-01 17:07:25 +0530

    added logout functionality

--- client/src/components/Chatlist/ChatListHeader.jsx
+++ client/src/components/Chatlist/ChatListHeader.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../common/Avatar";
-import { useStateProvider } from "@/context/StateContext";
 import { BsFillChatLeftTextFill, BsThreeDotsVertical } from "react-icons/bs";
+import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
+import { useRouter } from "next/router";
+import ContextMenu from "../common/ContextMenu";
 
-function ChatListHeader() {
+export default function ChatListHeader() {
   const [{ userInfo }, dispatch] = useStateProvider();
+  const router = useRouter();
+  const [contextMenuCordinates, setContextMenuCordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCordinates({ x: e.pageX, y: e.pageY });
+    setIsContextMenuVisible(true);
+  };
+
+  const contextMenuOptions = [
+    {
+      name: "Logout",
+      callback: async () => {
+        setIsContextMenuVisible(false);
+        router.push("/logout");
+      },
+    },
+  ];
 
   const handleAllContactsPage = () => {
     dispatch({ type: reducerCases.SET_ALL_CONTACTS_PAGE });
@@ -16,21 +40,29 @@ function ChatListHeader() {
       <div className="cursor-pointer">
         <Avatar type="sm" image={userInfo?.profileImage} />
       </div>
-      <div className="flex gap-6">
+      <div className="flex gap-6 ">
         <BsFillChatLeftTextFill
           className="text-panel-header-icon cursor-pointer text-xl"
-          title="New Chat"
+          title="New chat"
           onClick={handleAllContactsPage}
         />
         <>
           <BsThreeDotsVertical
             className="text-panel-header-icon cursor-pointer text-xl"
             title="Menu"
+            onClick={(e) => showContextMenu(e)}
+            id="context-opener"
           />
+          {isContextMenuVisible && (
+            <ContextMenu
+              options={contextMenuOptions}
+              cordinates={contextMenuCordinates}
+              contextMenu={isContextMenuVisible}
+              setContextMenu={setIsContextMenuVisible}
+            />
+          )}
         </>
       </div>
     </div>
   );
 }
-
-export default ChatListHeader;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -119,6 +119,13 @@ function Main() {
         });
       });
 
+      socket.current.on("online-users", ({ onlineUsers }) => {
+        dispatch({
+          type: reducerCases.SET_ONLINE_USERS,
+          onlineUsers,
+        });
+      });
+
       setSocketEvent(true);
     }
   }, [socket.current]);
