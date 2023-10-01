769e61851d14305c11b95e40a2cd857e3507e0e1
samarjit-singh <samarjitbravo@gmail.com>
2023-10-01 16:52:53 +0530

    added feature to exit the chat

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../common/Avatar";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { BiSearchAlt2 } from "react-icons/bi";
 import { MdCall } from "react-icons/md";
 import { IoVideocam } from "react-icons/io5";
-import { BiSearchAlt2 } from "react-icons/bi";
-import { BsThreeDotsVertical } from "react-icons/bs";
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
+import ContextMenu from "../common/ContextMenu";
 
-function ChatHeader() {
-  const [{ currentChatUser }, dispatch] = useStateProvider();
+export default function ChatHeader() {
+  const [{ userInfo, currentChatUser, onlineUsers }, dispatch] =
+    useStateProvider();
 
-  const handleVoiceCall = () => {
-    dispatch({
-      type: reducerCases.SET_VOICE_CALL,
-      voiceCall: {
-        ...currentChatUser,
-        type: "out-going",
-        callTypeL: "voice",
-        roomId: Date.now(),
-      },
-    });
+  const [contextMenuCordinates, setContextMenuCordinates] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCordinates({ x: e.pageX - 50, y: e.pageY + 20 });
+    setIsContextMenuVisible(true);
   };
 
+  const contextMenuOptions = [
+    {
+      name: "Exit",
+      callback: async () => {
+        setIsContextMenuVisible(false);
+        dispatch({ type: reducerCases.SET_EXIT_CHAT });
+      },
+    },
+  ];
 
   const handleVideoCall = () => {
     dispatch({
@@ -29,7 +40,19 @@ function ChatHeader() {
       videoCall: {
         ...currentChatUser,
         type: "out-going",
-        callTypeL: "video",
+        callType: "video",
+        roomId: Date.now(),
+      },
+    });
+  };
+
+  const handleVoiceCall = () => {
+    dispatch({
+      type: reducerCases.SET_VOICE_CALL,
+      voiceCall: {
+        ...currentChatUser,
+        type: "out-going",
+        callType: "audio",
         roomId: Date.now(),
       },
     });
@@ -41,10 +64,12 @@ function ChatHeader() {
         <Avatar type="sm" image={currentChatUser?.profilePicture} />
         <div className="flex flex-col">
           <span className="text-primary-strong">{currentChatUser?.name}</span>
-          <span className="text-secondary text-sm">online/offline</span>
+          <span className="text-secondary text-sm">
+            {onlineUsers.includes(currentChatUser.id) ? "online" : "offline"}
+          </span>
         </div>
       </div>
-      <div className="flex gap-6">
+      <div className="flex gap-6 ">
         <MdCall
           className="text-panel-header-icon cursor-pointer text-xl"
           onClick={handleVoiceCall}
@@ -55,12 +80,22 @@ function ChatHeader() {
         />
         <BiSearchAlt2
           className="text-panel-header-icon cursor-pointer text-xl"
-          onClick={() => dispatch({ type: reducerCases.SET_MESSAGE_SEARCH })}
+          onClick={() => dispatch({ type: reducerCases.SET_MESSAGES_SEARCH })}
         />
-        <BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" />
+        <BsThreeDotsVertical
+          className="text-panel-header-icon cursor-pointer text-xl"
+          onClick={(e) => showContextMenu(e)}
+          id="context-opener"
+        />
+        {isContextMenuVisible && (
+          <ContextMenu
+            options={contextMenuOptions}
+            cordinates={contextMenuCordinates}
+            contextMenu={isContextMenuVisible}
+            setContextMenu={setIsContextMenuVisible}
+          />
+        )}
       </div>
     </div>
   );
 }
-
-export default ChatHeader;
